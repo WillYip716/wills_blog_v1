1b86e6530ecfb0c6a4ac4689332477fc81044cbb
WillYip716 <william.yip.716@gmail.com>
2020-08-24 17:36:55 -0700

    post component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import {Route} from 'react-router-dom';
 import Home from './components/home';
+import Post from './components/post';
 
 function App() {
   return (
     <div>
       <Route path="/" exact component={Home} />
+      <Route path="/post/:id" exact component={Post} />
     </div>
   );
     
---
+++ client/src/components/post.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import axios from 'axios';
+
+
+class Post extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state ={
+            loading: true,
+            posts: []
+        };
+    }
+
+    componentDidMount() {
+        axios.get('/post/'+this.props.match.params.id)
+          .then(res => {
+            const posts = res.data;
+            this.setState((state) => ({
+                loading: false,
+                posts: posts
+            }));  
+          })
+    }
+
+    render(){
+        return(
+            <div>
+            {this.state.loading        
+                ? <h1>Post Loading</h1>
+                : <div>
+                    <h1>{this.state.posts.title}</h1>
+                    <p>{unescape(this.state.posts.article)}</p>
+                    <p>{this.state.posts.timestamp}</p> 
+                  </div>
+                  
+            }
+            
+                
+            </div>
+        )
+    }
+    
+}
+export default Post;
