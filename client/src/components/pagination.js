ed4d54286e3be9025673be9f1584f3f39e2b6ee5
WillYip716 <william.yip.716@gmail.com>
2020-09-02 18:04:08 -0700

    more pagination tests

--- client/src/components/pagination.js
+++ client/src/components/pagination.js
@@ -1,20 +1,42 @@
 import React from 'react';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 
 class Pagination extends React.Component {
     //this.props.name
 
+    constructor(props){
+        super(props);
+        this.state ={
+            loading: true,
+            title: "",
+            article: "",
+            timestamp: "",
+            published: ""
+        };
+    }
+
+
     render(){
         return (
 
-            <div >  
-                <a className="prev">{'<'}</a>
-                <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <a className="dropdown-item" href="#">Action</a>
-                    <a className="dropdown-item" href="#">Another action</a>
-                    <a className="dropdown-item" href="#">Something else here</a>
-                </div>
-                <a className="next">{'>'}</a>
+            <div className="pagenav">
+                <div style={{margin:"auto"}}>
+                    <a className="prev">{'<'}</a>
+                    <Dropdown>
+                        <Dropdown.Toggle variant="Secondary" id="dropdown-basic">
+                            1
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            <Dropdown.Item href="#/action-1">2</Dropdown.Item>
+                            <Dropdown.Item href="#/action-2">3</Dropdown.Item>
+                            <Dropdown.Item href="#/action-3">4</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                    <p style={{display:"inline-block"}}>{"/" + "10"}</p>
+                    <a className="next">{'>'}</a>
+                </div>  
+                
             </div>  
     
         )
