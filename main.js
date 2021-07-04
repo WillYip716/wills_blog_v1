7dace6a8e091fc96830a0d2abdd682311c6c96d6
WillYip716 <william.yip.716@gmail.com>
2021-07-04 14:07:42 -0700

    tinkering with CSP

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const express = require("express");
 require("dotenv").config(); // for loading environment variables
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const path = require("path");
 const app = express();
 
 var indexRouter = require('./routes/index');
@@ -16,6 +17,13 @@ const MONGO_URI = process.env.MONGODB_URI||process.env.MONGO_URI;
 mongoose.connect(MONGO_URI, { useNewUrlParser: true })
 .then(() => console.log("Mongo Connection successful"))
 .catch(err => console.log("err"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
